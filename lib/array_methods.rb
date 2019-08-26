d93a9c74adcec84e017a4a1cc275a157491b6b21
Dean Hildebrand <dean.hildebrand88@gmail.com>
2019-08-26 16:39:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,27 +1,27 @@
-# def find_element_index(array, value_to_find)
-# counter = 0
+def find_element_index(array, value_to_find)
+counter = 0
  
-# while counter < array.length do 
+while counter < array.length do 
    
    
-#   if array[counter] == value_to_find
-#   return counter
+  if array[counter] == value_to_find
+  return counter
   
-# end
-# counter += 1
-# end
-# end
-
-
-def find_max_value(array)
-  counter = 0
-  
-  while counter < array.length do
-   counter += 1
-  end
-  
- return array.max
 end
+counter += 1
+end
+end
+
+
+# def find_max_value(array)
+#   counter = 0
+  
+#   while counter < array.length do
+#   counter += 1
+#   end
+  
+# return array.max
+# end
 
 
 
